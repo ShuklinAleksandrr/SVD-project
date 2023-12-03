e7331a1f18d50649c913dec959c8d91b5e43c15f
Danil Kulibaba <70916051+MathPerv@users.noreply.github.com>
2023-12-03 15:14:56 +0300

    Update generate_svd.cpp

--- src/generate_svd.cpp
+++ src/generate_svd.cpp
@@ -17,11 +17,9 @@ using namespace Eigen;
 //n - число столбцов
 //lmbd - массив сингулярных значений
 template <typename T,int M, int N>
-tuple<Eigen::Matrix<T, M, M>, Eigen::Matrix<T, M, N>, Eigen::Matrix<T, N, N>> generate_svd()
+tuple<Eigen::Matrix<T, M, M>, Eigen::Matrix<T, M, N>, Eigen::Matrix<T, N, N>> matrices()
 {
     tuple<Eigen::Matrix<T, M, M>, Eigen::Matrix<T, M, N>, Eigen::Matrix<T, N, N>> tuple_matrix;
-    
-    int K = (M <= N) ? M : N;
     
     //U,V-ортогональные матрицы, SIGMA - диагональная матрица
     
@@ -31,53 +29,40 @@ tuple<Eigen::Matrix<T, M, M>, Eigen::Matrix<T, M, N>, Eigen::Matrix<T, N, N>> ge
     
     Matrix<T,Dynamic,Dynamic> T_1(M,M), T_2(N,N), Q_1(M,M), Q_2(N,N), R_1(M,M), R_2(N,N), U(M,M), V_T(N,N), SIGMA(M,N);
     
+    int K = (M <= N) ? M : N;
     T lmbd[K]; // lmbd - массив сингулярных значений размера K
     
     //Сингулярные значения нумеруются в порядке убывания
     //Тут на всякий случай сортируем массив сингулярных чисел, чтобы элменты шли в порядке убывания l1 >= l2 >=.....>= lk >= 0
     
-    Map<Vector<T,((M <= N) ? M : N)>> l(lmbd,K);
+    Map<Vector<T,Dynamic>> l(lmbd,K);
     
     //Пока такая сортировка
     std::sort(l.begin(), l.end(), std::greater_equal<float>());
-
+    
+    
     //Заполнение матрицы сигма
     //На диагонали стоят сингулярные значения, остальные элементы - 0
-    if (M<N)
-    {
-        SIGMA << Matrix<T,Dynamic,Dynamic>(l.asDiagonal()),
-        MatrixXf::Zero(K,N-K);
-    }
     
-    else
+    SIGMA.setZero();
+    
+    for(int i = 0; i < K; i++)
+        SIGMA(i,i) = l[i];
         
-        if (M>N)
-        {
-            SIGMA << Matrix<T,Dynamic,Dynamic>(l.asDiagonal()),
-            MatrixXf::Zero(M-K,N);
-        }
     
-       else //Квадратная матрица
-       {
-           SIGMA << Matrix<T,Dynamic,Dynamic>(l.asDiagonal());
-       }
+    //Заполнение матриц T_1 и T_2 случайными элементами от 0.1 до 10
+    float HI = 10;
+    float LO = 0.1;
+    float range= HI-LO;
     
-    T rnd_1[M*M];
+    T_1 = Matrix<T,Dynamic,Dynamic>::Random(M,M);
+    T_1 = (T_1 + Matrix<T,Dynamic,Dynamic>::Constant(M,M,1))*range/2;
+    T_1 = (T_1 + Matrix<T,Dynamic,Dynamic>::Constant(M,M,LO));
     
-    for (int i {0}; i<M*M; i++)
-        rnd_1[i] = (rand() % 101 + 1)/ 10.0;
+    T_2 = Matrix<T,Dynamic,Dynamic>::Random(N,N);
+    T_2 = (T_2 + Matrix<T,Dynamic,Dynamic>::Constant(N,N,1))*range/2;
+    T_2 = (T_2 + Matrix<T,Dynamic,Dynamic>::Constant(N,N,LO));
     
-    T rnd_2[N*N];
-    
-    for (int i {0}; i<N*N; i++)
-       rnd_2[i] = (rand() % 101 + 1)/ 10.0;
-
-
-    Map<Matrix<T,Dynamic,Dynamic>> t_1 (rnd_1,M,M);
-    Map<Matrix<T,Dynamic,Dynamic>>  t_2 (rnd_2,N,N);
-    
-    T_1 = t_1;
-    T_2 = t_2;
     
     //QR_разложение матриц T1 и T2
     Q_1 = (FullPivHouseholderQR<MatrixXf>(T_1)).matrixQ();
@@ -86,6 +71,7 @@ tuple<Eigen::Matrix<T, M, M>, Eigen::Matrix<T, M, N>, Eigen::Matrix<T, N, N>> ge
     V_T = Q_2;
     
     tuple_matrix = make_tuple(U,SIGMA,V_T);
+
     
     return tuple_matrix;
 
@@ -95,10 +81,12 @@ int main(int argc, const char * argv[]) {
     
     //int m, n, k;
     
-    // k = (m <= n) ? m : n; //k=min(m,n), равенство для квадратной матрицы
+    //k = (m <= n) ? m : n; //k=min(m,n), равенство для квадратной матрицы
     
     //Вызов функции пока закомментирован, так как нужны конкретные значения для m и n
-    generate_svd<float,4,3>();
+    //matrices<float,m,n>();
     
     return 0;
 }
+
+
